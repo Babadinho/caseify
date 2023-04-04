c6c11a94081e57fee5d0c2fa0c8fbeaf5018fc56
Donald Iloekwe <donytex4u@gmail.com>
2023-04-04 14:25:34 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+### Getting Started
+
+- `npm install` to install all dependencies
+- `npm run dev` to start server
+
+### Build command
+
+- `npm run build`
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: {
+    popup: './src/popup.tsx',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.css', '.scss'],
+    modules: ['src', 'node_modules'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript',
+            ],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/popup.html',
+      filename: 'popup.html',
+      chunks: ['index'],
+    }),
+  ],
+};
